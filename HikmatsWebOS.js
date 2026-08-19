845cdba4847498e03c94db02ed42165b62b486de
hikmatgara <259418730+hikmatgara@users.noreply.github.com>
2026-08-19 20:40:05 +0400

    Update HikmatsWebOS.js

--- HikmatsWebOS.js
+++ HikmatsWebOS.js
@@ -10,31 +10,56 @@ function updateTime() {
   var currentX = 0;
   var currentY = 0;
 
-// Make the DIV element draggable:
+
 dragElement(document.getElementById("window"));
 
-// Step 1: Define a function called `dragElement` that makes an HTML element draggable.
+
 function dragElement(element) {
-  // Step 2: Set up variables to keep track of the element's position.
+
   var initialX = 0;
   var initialY = 0;
   var currentX = 0;
   var currentY = 0;
 
-  // Step 3: Check if there is a special header element associated with the draggable element.
+  
   if (document.getElementById(element.id + "header")) {
-    // Step 4: If present, assign the `dragMouseDown` function to the header's `onmousedown` event.
-    // This allows you to drag the window around by its header.
+    
     document.getElementById(element.id + "header").onmousedown = startDragging;
   } else {
-    // Step 5: If not present, assign the function directly to the draggable element's `onmousedown` event.
-    // This allows you to drag the window by holding down anywhere on the window.
+    
     element.onmousedown = startDragging;
   }
 
-  // Step 6: Define the `startDragging` function to capture the initial mouse position and set up event listeners.
+  
   function startDragging(e) {
     e = e || window.event;
+    e.preventDefault();
+  
+    initialX = e.clientX;
+    initialY = e.clientY;
+   
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+  
+    e = e || window.event;
+    e.preventDefault();
+    
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }    e = e || window.event;
     e.preventDefault();
     // Step 7: Get the mouse cursor position at startup.
     initialX = e.clientX;
