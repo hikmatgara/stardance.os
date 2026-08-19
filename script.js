4bcf93c132a633facd6dae4e58088f2f138328de
hikmatgara <259418730+hikmatgara@users.noreply.github.com>
2026-08-19 22:23:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+
+var welcomeScreen = document.querySelector("#welcome");
+var closeButton = document.querySelector("#welcomeclose");
+var openButton = document.querySelector("#welcomeopen");
+
+closeButton.addEventListener("click", function() {
+    welcomeScreen.style.display = "none";
+});
+
+openButton.addEventListener("click", function() {
+    welcomeScreen.style.display = "block";
+});
+
+function updateTime() {
+  var HikmatOScurrentTime = new Date().toLocaleString();
+    var HikmatOStimeText = document.querySelector("#timeElement");
+    HikmatOStimeText.innerHTML = HikmatOScurrentTime;
+    }
+    setInterval(function() {
+      document.querySelector("#timeElement").innerHTML = new Date().toLocaleString();
+    }, 1000);
+    var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+
+dragElement(document.getElementById("window"));
+
+
+function dragElement(element) {
+
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+  
+  if (document.getElementById(element.id + "header")) {
+    
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+    
+    element.onmousedown = startDragging;
+  }
+
+  
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+  
+    initialX = e.clientX;
+    initialY = e.clientY;
+   
+    document.onmouseup = stopDragging;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
